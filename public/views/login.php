a9f7a50513f828fadeec1df10d881d9050cd9849
JakubKw19 <kkwiecie@interia.pl>
2025-01-21 13:51:49 +0100

    Set up a new project and added styled login page

---
+++ public/views/login.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+
+<head>
+    <link rel="preconnect" href="https://rsms.me/">
+    <link rel="stylesheet" href="https://rsms.me/inter/inter.css">
+    <link rel="stylesheet" type="text/css" href="public/css/style.css">
+    <title>LOGIN PAGE</title>
+</head>
+
+<body>
+    <div class="container">
+        <div class="ellipsis">
+            <img class="logotype" src="public/img/logotype.svg" alt="logotype">
+        </div>
+        <div class="login-container">
+            <h1 class="signin">Sign In</h1>
+            <form>
+                <div class="input-container">
+                    <label for="email">Email</label>
+                    <input name="email" type="text" placeholder="email@email.com">
+                </div>
+                <div class="input-container">
+                    <label for="password">Password</label>
+                    <input name="password" type="password" placeholder="Password">
+                </div>
+                <div class="input-container">
+                    <button class="login-button">CONTINUE</button>
+                    <button class="fb-button">WITH FACEBOOK</button>
+                </div>
+            </form>
+        </div>
+    </div>
+</body>
